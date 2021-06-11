e65d8ddcc73c03d8b081929d23181f9bb9b04e7c
VenusCese <85318305+VenusCese@users.noreply.github.com>
2021-06-10 20:52:02 -0700

    Add files via upload

---
+++ Proyecto Final/Validador.php
@@ -0,0 +1,26 @@
+<?php
+
+$nom = $_GET['nombre'];
+$apep = $_GET['apellidop'];
+$apem = $_GET['apellidom'];
+$dia = $_GET['dia'];
+$mes = $_GET['mes'];
+$año = $_GET['año'];
+$eda = $_GET['edad'];
+$grup = $_GET['grupo'];
+$corre = $_GET['correo'];
+$contra = $_GET['contraseña'];
+
+
+echo $nom."<br>";
+echo $apep."<br>";
+echo $apem."<br>";
+echo $dia."<br>";
+echo $mes."<br>";
+echo $año."<br>";
+echo $eda."<br>";
+echo $grup."<br>";
+echo $corre."<br>";
+echo $contra."<br>";
+
+?>
